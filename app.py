1d0db3e87618c64f1d89f4c90a448e3a55be99b5
TU7SHAR <brahmin00011@gmail.com>
2026-03-12 13:39:48 +0530

    Render+Aiven

--- app.py
+++ app.py
@@ -3,22 +3,24 @@ import urllib.parse
 from flask import Flask, render_template
 from models import db
 from auth import auth_bp
-from tasks import tasks_bp # Assuming you put Todo routes in tasks.py
+from tasks import tasks_bp 
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config['WTF_CSRF_ENABLED'] = False
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'mysecret')
 
 user = os.getenv('MYSQL_USER')
 password = urllib.parse.quote_plus(os.getenv('MYSQL_PASSWORD'))
 host = os.getenv('MYSQL_HOST')
+port = os.getenv('MYSQL_PORT', '17449')
 database = os.getenv('MYSQL_DB')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', f'mysql://{user}:{password}@{host}/{database}')
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}?ssl_disabled=False"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"pool_pre_ping": True}
 
 db.init_app(app)
 
@@ -32,4 +34,4 @@ def index():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
